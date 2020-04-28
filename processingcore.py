6b7b3e2d13bf1e6d8f6ac336b46fb1da2f669795
Anthony Fong <FongAnthonyM@gmail.com>
2020-04-28 15:49:12 -0700

    separated logging out

--- processingcore.py
+++ processingcore.py
@@ -13,13 +13,7 @@ __email__ = ""
 __status__ = "Prototype"
 
 # Default Libraries #
-import abc
 import asyncio
-import copy
-import datetime
-import logging
-import logging.config
-import logging.handlers
 import multiprocessing
 from multiprocessing import Process, Pool, Lock, Event, Queue, Pipe
 import queue
@@ -30,251 +24,11 @@ import time
 
 
 # Local Libraries #
+import advancelogging
 
 
 # Definitions #
-# Logger #
-# Child logging.getLogger("myapp.ui")
-
 # Classes #
-class ObjectInheritor(abc.ABC):
-    _attributes_as_parents = []
-
-    def __copy__(self):
-        new = type(self)()
-        new.__dict__.update(self.__dict__)
-        return new
-
-    def __deepcopy__(self, memo={}):
-        new = type(self)()
-        for attribute in self._attributes_as_parents:
-            if attribute in dir(self):
-                parent_object = copy.deepcopy(super().__getattribute__(attribute))
-                setattr(new, attribute, parent_object)
-        new.__dict__.update(self.__dict__)
-        return new
-
-    # Attribute Access
-    def __getattribute__(self, name):
-        # Check if item is in self and if not check in the object parents
-        if name[0:2] != "__" and name not in {"_attributes_as_parents"} and \
-           name not in self._attributes_as_parents and name not in dir(self):
-            # Iterate through all object parents to find attribute
-            for attribute in self._attributes_as_parents:
-                parent_object = super().__getattribute__(attribute)
-                if name in dir(parent_object):
-                    return getattr(parent_object, name)
-
-        # If the item is an attribute in self or not in any object parent return attribute
-        return super().__getattribute__(name)
-
-    def __setattr__(self, name, value):
-        # Check if item is in self and if not check in object parents
-        if name not in self._attributes_as_parents and name not in dir(self):
-            # Iterate through all indirect parents to find attribute
-            for attribute in self._attributes_as_parents:
-                if attribute in dir(self):
-                    parent_object = super().__getattribute__(attribute)
-                    if name in dir(parent_object):
-                        return setattr(parent_object, name, value)
-
-        # If the item is an attribute in self or not in any indirect parent set as attribute
-        return super().__setattr__(name, value)
-
-    def _setattr(self, name, value):
-        super().__setattr__(name, value)
-
-
-class PreciseFormatter(logging.Formatter):
-    converter = datetime.datetime.fromtimestamp
-    default_msec_format = "%s.%06d"
-
-    def formatTime(self, record, datefmt=None):
-        ct = self.converter(record.created)
-        if datefmt:
-            s = ct.strftime(datefmt)
-        else:
-            t = ct.strftime(self.default_time_format)
-            s = self.default_msec_format % (t, ct.microsecond)
-        return s
-
-
-class AdvanceLogger(ObjectInheritor):
-    _attributes_as_parents = ["_logger"]
-
-    @classmethod
-    def from_config(cls, name, fname, defaults=None, disable_existing_loggers=True, **kwargs):
-        logging.config.fileConfig(fname, defaults, disable_existing_loggers)
-        return cls(name, **kwargs)
-
-    # Construction/Destruction
-    def __init__(self, obj=None, module_of_class="(Not Given)", init=True):
-        self.allow_append = False
-
-        self._logger = None
-        self.module_of_class = module_of_class
-        self.module_of_object = "(Not Given)"
-        self.append_message = ""
-
-        if init:
-            self.construct(obj)
-
-    @property
-    def name_parent(self):
-        return self.name.rsplit('.', 1)[0]
-
-    @property
-    def name_stem(self):
-        return self.name.rsplit('.', 1)[0]
-
-    # Pickling
-    def __getstate__(self):
-        out_dict = self.__dict__.copy()
-        out_dict["disabled"] = self.disabled
-        out_dict["level"] = self.getEffectiveLevel()
-        out_dict["propagate"] = self.propagate
-        out_dict["filters"] = copy.deepcopy(self.filters)
-        out_dict["handlers"] = []
-        for handler in self.handlers:
-            lock = handler.__dict__.pop("lock")
-            stream = handler.__dict__.pop("stream")
-            out_dict["handlers"].append(copy.deepcopy(handler))
-            handler.__dict__["lock"] = lock
-            handler.__dict__["stream"] = stream
-        return out_dict
-
-    def __setstate__(self, in_dict):
-        in_dict["_logger"].disabled = in_dict.pop("disabled")
-        in_dict["_logger"].setLevel(in_dict.pop("level"))
-        in_dict["_logger"].propagate = in_dict.pop("propagate")
-        in_dict["_logger"].filters = in_dict.pop("filters")
-        in_dict["_logger"].handlers = _rebuild_handlers(in_dict.pop("handlers"))
-        self.__dict__ = in_dict
-
-    # Constructors/Destructors
-    def construct(self, obj=None):
-        if isinstance(obj, logging.Logger):
-            self._logger = obj
-        else:
-            self._logger = logging.getLogger(obj)
-
-    # Logger Editing
-    def set_logger(self, logger):
-        self._logger = logger
-
-    def fileConfig(self, name, fname, defaults=None, disable_existing_loggers=True):
-        logging.config.fileConfig(fname, defaults, disable_existing_loggers)
-        self._logger = logging.getLogger(name)
-
-    def copy_logger_attributes(self, logger):
-        logger.propagate = self.propagate
-        logger.setLevel(self.getEffectiveLevel())
-        for filter_ in self.filters:
-            logger.addFilter(filter_)
-        for handler in self.handlers:
-            logger.addHandler(handler)
-        return logger
-
-    def setParent(self, parent):
-        new_logger = parent.getChild(self.name_stem)
-        self.copy_logger_attributes(new_logger)
-        self._logger = new_logger
-
-    # Defaults
-    def append_module_info(self):
-        self.append_message = "Class' Module: %s Object Module: %s " % (self.module_of_class, self.module_of_object)
-        self.allow_append = True
-
-    def add_default_stream_handler(self, level=logging.DEBUG):
-        handler = logging.StreamHandler()
-        handler.setLevel(level)
-        formatter = PreciseFormatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        self.addHandler(handler)
-
-    # Override Logger Methods
-    def debug(self, msg, *args, append=None, **kwargs):
-        if append or (append is None and self.allow_append):
-            msg = self.append_message + msg
-        self._logger.debug(msg, *args, **kwargs)
-
-    def info(self, msg, *args, append=None, **kwargs):
-        if append or (append is None and self.allow_append):
-            msg = self.append_message + msg
-        self._logger.info(msg, *args, **kwargs)
-
-    def warning(self, msg, *args, append=None, **kwargs):
-        if append or (append is None and self.allow_append):
-            msg = self.append_message + msg
-        self._logger.warning(msg, *args, **kwargs)
-
-    def error(self, msg, *args, append=None, **kwargs):
-        if append or (append is None and self.allow_append):
-            msg = self.append_message + msg
-        self._logger.error(msg, *args, **kwargs)
-
-    def critical(self, msg, *args, append=None, **kwargs):
-        if append or (append is None and self.allow_append):
-            msg = self.append_message + msg
-        self._logger.critical(msg, *args, **kwargs)
-
-    def log(self, level, msg, *args, append=None, **kwargs):
-        if append or (append is None and self.allow_append):
-            msg = self.append_message + msg
-        self._logger.critical(level, msg, *args, **kwargs)
-
-    def exception(self, msg, *args, append=None, **kwargs):
-        if append or (append is None and self.allow_append):
-            msg = self.append_message + msg
-        self._logger.exception(msg, *args, **kwargs)
-
-
-class ObjectWithLogging(abc.ABC):
-    class_loggers = {}
-
-    def __init__(self):
-        self.loggers = self.class_loggers.copy()
-
-    # Logging
-    def traceback_formatting(self, func, msg, name=""):
-        return "%s(%s) -> %s: %s" % (self.__class__, name, func, msg)
-
-
-def _rebuild_handlers(handlers):
-    new_handlers = []
-    for handler in handlers:
-        if isinstance(handler, logging.handlers.QueueHandler):
-            kwargs = {"queue", handler.queue}
-        elif isinstance(handler, logging.handlers.BufferingHandler):
-            kwargs = {"capacity": handlers.capacity}
-        elif isinstance(handler, logging.handlers.HTTPHandler):
-            kwargs = {"host": handler.host, "url": handler.url, "method": handler.method, "secure": handler.secure,
-                      "credentials": handler.credentials, "context": handler.context}
-        elif isinstance(handler, logging.handlers.NTEventLogHandler):
-            kwargs = {"appname": handler.appname, "dllname": handler.dllname, "logtype": handler.logtype}
-        elif isinstance(handler, logging.handlers.SMTPHandler):
-            kwargs = {"mailhost": handler.mailhost, "fromaddr": handler.fromaddr, "toaddrs": handler.toaddrs,
-                      "subject": handler.subject, "credentials": handler.credentials, "secure": handler.secure,
-                      "timeout": handler.timeout}
-        elif isinstance(handler, logging.handlers.SysLogHandler):
-            kwargs = {"address": handler.address, "facility": handler.facility, "socktype": handler.socktype}
-        elif isinstance(handler, logging.handlers.SocketHandler):
-            kwargs = {"host": handler.host, "port": handler.port}
-        elif isinstance(handler, logging.FileHandler):
-            kwargs = {"filename": handler.baseFilename, "mode": handler.mode,
-                      "encoding": handler.encoding, "delay": handler.delay}
-        elif isinstance(handler, logging.StreamHandler):
-            kwargs = {}
-            # warnings.warn("StreamHandler stream cannot be pickled, using default stream")
-        else:
-            warnings.warn()
-            continue
-        new_handler = type(handler)(**kwargs)
-        new_handler.__dict__.update(handler.__dict__)
-        new_handlers.append(new_handler)
-    return new_handlers
-
-
 class Interrupt(object):
     def __init__(self, master=None):
         self.master = master
@@ -1001,8 +755,8 @@ class OutputsHandler(object):
         self.interrupts.interrupt_all_processes()
 
 
-class Task(ObjectWithLogging):
-    class_loggers = {"task_root": AdvanceLogger("task_root")}
+class Task(advancelogging.ObjectWithLogging):
+    class_loggers = {"task_root": advancelogging.AdvanceLogger("task_root")}
 
     # Construction/Destruction
     def __init__(self, name=None, allow_setup=True, allow_closure=True,
@@ -1503,10 +1257,10 @@ class MultiUnitTask(Task):
             self.units[name].reset()
 
 
-class SeparateProcess(ObjectInheritor, ObjectWithLogging):
+class SeparateProcess(advancelogging.ObjectInheritor, advancelogging.ObjectWithLogging):
     _attributes_as_parents = ["_process"]
     CPU_COUNT = multiprocessing.cpu_count()
-    class_loggers = {"separate_process": AdvanceLogger("separate_process")}
+    class_loggers = {"separate_process": advancelogging.AdvanceLogger("separate_process")}
 
     # Construction/Destruction
     def __init__(self, target=None, name=None, daemon=False, init=False, kwargs={}):
@@ -1644,9 +1398,9 @@ class SeparateProcess(ObjectInheritor, ObjectWithLogging):
             self.process.close()
 
 
-class ProcessingUnit(ObjectInheritor, ObjectWithLogging):
+class ProcessingUnit(advancelogging.ObjectInheritor, advancelogging.ObjectWithLogging):
     _attributes_as_parents = ["_task_object"]
-    class_loggers = {"processor_root": AdvanceLogger("processor_root")}
+    class_loggers = {"processor_root": advancelogging.AdvanceLogger("processor_root")}
     DEFAULT_TASK = Task
 
     # Construction/Destruction
